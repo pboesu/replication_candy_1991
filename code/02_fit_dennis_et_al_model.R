cbbe17ce63373b14f64e4e4ef9dc3794a212943f
Philipp Boersch-Supan <philipp@boersch-supan.de>
2020-09-03 00:08:18 +0100

    tidied up code and added figures for dennis model

--- code/02_fit_dennis_et_al_model.R
+++ code/02_fit_dennis_et_al_model.R
@@ -1,67 +1,4 @@
-P <- (stage==1)*1/(1+exp(-(A1-ddeg)/sqrt(BB*ddeg)))+
-  (stage==2)*1/(1+exp(-(A2-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A1-ddeg)/sqrt(BB*ddeg)))+
-  (stage==3)*1/(1+exp(-(A3-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A2-ddeg)/sqrt(BB*ddeg)))+
-  (stage==4)*1/(1+exp(-(A4-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A3-ddeg)/sqrt(BB*ddeg)))+
-  (stage==5)*1/(1+exp(-(A5-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A4-ddeg)/sqrt(BB*ddeg)))+
-  (stage==6)*1/(1+exp(-(A6-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A5-ddeg)/sqrt(BB*ddeg)))+
-  (stage==7)*1/(1+exp((A6-ddeg)/sqrt(BB*ddeg)))
-
-
-
-nls(count ~ total*(stage==1)*1/(1+exp(-(A1-ddeg)/sqrt(BB*ddeg)))+
-  (stage==2)*1/(1+exp(-(A2-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A1-ddeg)/sqrt(BB*ddeg)))+
-  (stage==3)*1/(1+exp(-(A3-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A2-ddeg)/sqrt(BB*ddeg)))+
-  (stage==4)*1/(1+exp(-(A4-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A3-ddeg)/sqrt(BB*ddeg)))+
-  (stage==5)*1/(1+exp(-(A5-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A4-ddeg)/sqrt(BB*ddeg)))+
-  (stage==6)*1/(1+exp(-(A6-ddeg)/sqrt(BB*ddeg)))-1/(1+exp(-(A5-ddeg)/sqrt(BB*ddeg)))+
-  (stage==7)*1/(1+exp((A6-ddeg)/sqrt(BB*ddeg))), data = budworm_counts, start = list(A1 = 150, A2 = 230, A3 = 280, A4 = 330, A5 = 440, A6 = 580, BB = 2), lower = c(100,150,230,280,330,440,0.5), upper = c(600,600,600,600,600,600,3), trace = TRUE, algorithm = 'port', control = nls.control(minFactor = 1/4096, maxiter = 200)) 
-
-#optim
-# wfn<-function(par, Cfl, Tsoil, Pw){
-#   K<-par[1]
-#   a<-par[2]
-#   b<-par[3]
-#   c<-par[4]
-#   z<-par[5]
-#   m<-length(Pw)
-#   res<-rep(NA,m)
-#   for (i in 1:m){
-#     tmp<-1
-#     if (Pw[i] <= z) tmp<-exp(-a*Tsoil[i])
-#     ff<-K*tmp*exp(-b*Pw[i])+c
-#     res[i]<-Cfl[i]-ff
-#   }
-#   as.numeric(crossprod(res))
-# }
-#a1<-optim(st,wfn, control=list(trace=1), Cfl=Cfl, Tsoil=Tsoil, Pw=Pw)
-weighted_residual <- function(par, count, total, stage, ddeg){
-  A1 <- par[1]
-  A2 <- par[2]
-  A3 <- par[3]
-  A4 <- par[4]
-  A5 <- par[5]
-  A6 <- par[6]
-  BB <- par[7]
-  pred <- weight <- rep(NA, length(total))
-  for (i in seq_along(total)){
-    pred[i] <- (stage[i]==1)*1/(1+exp(-(A1-ddeg[i])/sqrt(BB*ddeg[i])))+
-    (stage[i]==2)*(1/(1+exp(-(A2-ddeg[i])/sqrt(BB*ddeg[i])))-1/(1+exp(-(A1-ddeg[i])/sqrt(BB*ddeg[i]))))+
-    (stage[i]==3)*(1/(1+exp(-(A3-ddeg[i])/sqrt(BB*ddeg[i])))-1/(1+exp(-(A2-ddeg[i])/sqrt(BB*ddeg[i]))))+
-    (stage[i]==4)*(1/(1+exp(-(A4-ddeg[i])/sqrt(BB*ddeg[i])))-1/(1+exp(-(A3-ddeg[i])/sqrt(BB*ddeg[i]))))+
-    (stage[i]==5)*(1/(1+exp(-(A5-ddeg[i])/sqrt(BB*ddeg[i])))-1/(1+exp(-(A4-ddeg[i])/sqrt(BB*ddeg[i]))))+
-    (stage[i]==6)*(1/(1+exp(-(A6-ddeg[i])/sqrt(BB*ddeg[i])))-1/(1+exp(-(A5-ddeg[i])/sqrt(BB*ddeg[i]))))+
-    (stage[i]==7)*(1/(1+exp((A6-ddeg[i])/sqrt(BB*ddeg[i]))))
-  }
-  pred <- pmax(pred, 1e-7)
-  res <- (count - total*pred)^2
-  weight <- 1/(total*pred)
-  return(sum(weight*res))
-}
-weighted_residual(par = c(A1 = 150, A2 = 230, A3 = 280, A4 = 330, A5 = 440, A6 = 580, BB = 2), count = budworm_counts$count, total = budworm_counts$total, stage = budworm_counts$stage, ddeg = budworm_counts$ddeg)
-weighted_residual(par = c(A1 = 120, A2 = 204, A3 = 264, A4 = 342, A5 = 465, A6 = 599, BB = 1.5), count = budworm_counts$count, total = budworm_counts$total, stage = budworm_counts$stage, ddeg = budworm_counts$ddeg)
-
-a1 <- optim(par = c(A1 = 150, A2 = 230, A3 = 280, A4 = 330, A5 = 440, A6 = 580, BB = 1), weighted_residual, count = budworm_counts$count, total = budworm_counts$total, stage = budworm_counts$stage, ddeg = budworm_counts$ddeg, hessian = TRUE, control = list(trace=1), method = 'BFGS')
-
+budworm_counts <- readr::read_csv('data/budworm_counts.csv')
 
 #optimize likelihood directly rather than doing the IRLS approach
 neg_log_lik <- function(par, count, total, stage, ddeg){
@@ -109,14 +46,43 @@ predicted_proportion <- function(par, stage, ddeg){
   }
   return(pred)}
 
+
 #predictions and comparisons
 dennis_par = c(A1 = 121.08, A2 = 204.36, A3 = 264.41, A4 = 342.473, A5 = 465.620, A6 = 599.57, BB = 1.559)
-sas_par = c(120.0,	204.7,	264.6,	341.3,	464.5,	595.7,	1.4119)
-newdata <- expand.grid(stage = 1:7, ddeg = 0:900)
-newdata$model_predictions <- predicted_proportion(a2$par, newdata$stage, newdata$ddeg)
-newdata$original_prediction <- predicted_proportion(dennis_par, newdata$stage, newdata$ddeg)
-newdata$sas_prediction <- predicted_proportion(sas_par, newdata$stage, newdata$ddeg)
+sas_par = c(120.0, 204.7, 264.6, 341.3, 464.5, 595.7, 1.4119)
 
-ggplot(newdata, aes(x = ddeg, y = model_predictions, col = factor(stage), group = stage)) + geom_line() + geom_point(data = budworm_counts, aes(y = count/total)) + theme_classic() + geom_line(aes(y = original_prediction), lty=2) + geom_line(aes(y = sas_prediction), lty=3,col='black')
-
-ggplot(newdata, aes(x = ddeg, y = model_predictions, group = stage)) + geom_line() + geom_point(data = budworm_counts, aes(y = count/total)) + facet_wrap(~stage) + theme_classic()
+#figure 2
+logistic_pdf <- function(s, t, bb){
+  exp((s-t)/(sqrt(bb*t)))/(sqrt(bb*t)*(1+exp((s-t)/(sqrt(bb*t))))^2)
+}
+s <- 0:800
+#redraw fig 2 using paramter estimates from Kemp
+pdf('figures/dennis_fig2.pdf', width = 10, height = 7)
+plot(s, logistic_pdf(s, 50, 1.559), type='l', ylab = "Probaility density", ylim = c(0,0.031), xaxs = 'i', yaxs='i')
+for (t in c(150,250,350,450,550,650)){
+  lines(s, logistic_pdf(s, t, 1.559))
+}
+abline(v = dennis_par[1:6], lty=2)
+#overplot replication results
+lines(s, logistic_pdf(s, 50, a2$par[7]), type='l', col = '#fc8d59')
+for (t in c(150,250,350,450,550,650)){
+  lines(s, logistic_pdf(s, t, a2$par[7]), col = "#fc8d59")
+}
+abline(v = a2$par[1:6], lty=2, col = "#fc8d59")
+legend('topright',lty = 1, col = c('black','#fc8d59'), legend = c('Kemp et al. 1986', 'Replication'), bty = 'n')
+text(dennis_par[1:6]+10, 0.030, latex2exp::TeX(c('a_1','a_2','a_3','a_4','a_5','a_6')))
+text(c(50,150,250,350,450,550,650),c(0.0305,0.018,0.014,0.012,0.011,0.01,0.009), paste('t =',c(50,150,250,350,450,550,650)))
+dev.off()
+#redraw fig 3
+pdf('figures/dennis_fig3.pdf', width = 10, height = 7)
+plot(s,predicted_proportion(dennis_par,rep(1, length(s)),s), type = 'l', ylab = 'Proportion in stage', xlab = 'Time (degree-days)')
+for (a in 2:7){
+  lines(s,predicted_proportion(dennis_par,rep(a, length(s)),s))
+}
+lines(s,predicted_proportion(a2$par,rep(1, length(s)),s), col = "#fc8d59")
+for (a in 2:7){
+  lines(s,predicted_proportion(a2$par,rep(a, length(s)),s), col = "#fc8d59")
+}
+legend('top',lty = 1, col = c('black','#fc8d59'), legend = c('Kemp et al. 1986', 'Replication'), bty = 'n')
+text(c(90,160,233,300,400,530,690), c(0.98,0.91,0.7,0.75,0.89,0.86,0.98), 1:7)
+dev.off()
